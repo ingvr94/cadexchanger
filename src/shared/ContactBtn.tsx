e36beacbc1fb553d3ba8f9bfee75a4874dd6f81d
ingvr94 <ingvar94@mail.ru>
2024-10-31 22:06:44 +0300

    delete reload page

--- src/shared/ContactBtn.tsx
+++ src/shared/ContactBtn.tsx
@@ -5,7 +5,7 @@ const ContactBtn = () => {
     <button
     className="middle none rounded-lg bg-gray-900 py-2 px-12 text-center align-middle  text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
   >
-    <Link to='/contactus' reloadDocument>
+    <Link to='/contactus' >
         Contact us
     </Link>
   </button>
